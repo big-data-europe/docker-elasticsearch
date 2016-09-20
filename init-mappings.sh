98609a9ecd52aadabda3a46a96c8cd4cf0b24953
Mohamed Nadjib MAMI <mohamed.nadjib.mami@gmail.com>
2016-09-20 19:00:29 +0200

    Update init-mappings.sh

--- init-mappings.sh
+++ init-mappings.sh
@@ -11,7 +11,7 @@ BASENAME=$(basename $file_url)
 
 for i in {30..0}; do
     if curl elasticsearch:9200; then
-		curl -XPUT "elasticsearch:9200/$INDEXNAME"
+	curl -XPUT "elasticsearch:9200/$INDEXNAME"
         curl -XPUT "elasticsearch:9200/$INDEXNAME/_mapping/$MAPPINGSNAME" -d @"$BASENAME"
         break;
     fi
